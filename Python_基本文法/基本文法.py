88a734ea956d1f7857781ba808f3e7494907cffc
ARAI Hideaki <a.hide0205@gmail.com>
2024-02-14 21:49:29 +0900

    0214 Python基本文法のフォルダ追加 演習した文法も更新

---
+++ Python_基本文法/基本文法.py
@@ -0,0 +1,35 @@
+print("Hello World!")
+
+
+#文字列
+#ーーーーーーーーー
+print("1 + 1")
+
+#数字の計算
+print(1 + 1)
+#ーーーーーーーーー
+
+
+#文字の改行
+#ーーーーーーーーーーー
+print("hello!")
+print("world!")
+#ーーーーーーーーーーー
+
+
+#実行結果の間隔をあけてみる
+print("あああ","いいい")
+
+#変数に文字列を代入
+name = "太郎"
+print(name)
+
+
+#変数代入の上書き
+name = "一太郎"
+print(name)
+
+name = "二太郎"
+print(name)
+
+
